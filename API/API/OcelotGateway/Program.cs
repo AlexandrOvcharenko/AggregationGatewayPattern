69be36febedbe424c52cab7da7772d1906e319f0
Alexandr Ovcharenko <a.ovcharenko@creatio.com>
2022-09-20 14:15:03 +0300

    Added QoS by using Circuit breaker pattern

--- API/API/OcelotGateway/Program.cs
+++ API/API/OcelotGateway/Program.cs
@@ -1,24 +1,16 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Host.ConfigureAppConfiguration((hostingContext, config) => {
-	var env = hostingContext.HostingEnvironment;
-	config.AddJsonFile($"ocelot.{env.EnvironmentName}.json",
-						   optional: false,
-						   reloadOnChange: true);
-});
-
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot()
+				.AddPolly();
 
 var app = builder.Build();
 
@@ -30,10 +22,9 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
-await app.UseOcelot();
+app.UseOcelot().Wait();
 
-app.UseAuthorization();
+app.Run();
 
-app.MapControllers();
 
- app.Run();
+
